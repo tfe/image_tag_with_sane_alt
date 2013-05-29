3ae6f4fcf9f9d7ee31724fe7f6f1463b6e61e5a3
Todd Eichel <todd@toddeichel.com>
2013-05-29 10:06:09 -0700

    Oops, didn't fully propagate that rename.

--- lib/image_tag_with_sane_alt/railtie.rb
+++ lib/image_tag_with_sane_alt/railtie.rb
@@ -2,7 +2,7 @@ module ImageTagWithSaneAlt
   class Railtie < Rails::Railtie
     initializer 'image_tag_with_sane_alt.configure' do
       ActiveSupport.on_load :action_view do
-        require 'image_tag_with_sane_alt/asset_tag_patch'
+        require 'image_tag_with_sane_alt/image_tag_patch'
       end
     end
   end
